0fcc4b759761f5c632d4d57c30a39540a035dc0a
RachmanForniandi <rachmanforniandi@gmail.com>
2020-01-14 22:59:14 +0700

    implement local source data source methods

--- app/src/main/java/com/example/githubrepoapp/RxApp.kt
+++ app/src/main/java/com/example/githubrepoapp/RxApp.kt
@@ -1,9 +1,16 @@
 package com.example.githubrepoapp
 
 import android.app.Application
+import android.content.Context
 
 class RxApp :Application() {
+    companion object {
+        lateinit var  INSTANCE:RxApp
+    }
+
+
     override fun onCreate() {
         super.onCreate()
+        INSTANCE = this
     }
 }
